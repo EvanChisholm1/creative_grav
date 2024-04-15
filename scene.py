47544467378bde26c1de18ca42bb15fdc8ff39e0
Evan Chisholm <elchisholm248@gmail.com>
2024-04-15 16:43:40 -0400

    update

--- scene.py
+++ scene.py
@@ -2,8 +2,8 @@ from manim import *
 
 class Orbit(Scene):
     def construct(self):
-        center_circle = Circle(radius=0.5, color=BLUE)
-        center_circle.set_fill(BLUE, opacity=0.5)
+        center_circle = Circle(radius=0.5, color=PINK)
+        center_circle.set_fill(PINK, opacity=0.5)
         # self.add(center_circle)
 
         # Create a second circle that will orbit around the center circle
@@ -17,8 +17,17 @@ class Orbit(Scene):
         
         self.play(
             # orbiting_circle.animate.orbit(2 * PI, about_point=center_circle.get_center()),
-            Rotate(mobject=orbiting_circle, about_point=(0.0, 0.0, 0.0), angle=2 * PI),
-            center_circle.animate.shift(LEFT),
+            Rotate(mobject=orbiting_circle, about_point=(0.0, 0.0, 0.0), angle=4 * PI),
             run_time=5,
             rate_func=linear,
+        )
+
+        self.play(
+            center_circle.animate.shift(2 * RIGHT),
+            orbiting_circle.animate.shift(2 * RIGHT),
+        )
+
+        uni_grav_formula = MathTex(r"\vec{F_ag}")
+        self.play(
+            Create(uni_grav_formula)
         )
